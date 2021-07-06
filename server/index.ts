bfaad57b10b744277411d0792257b3f6c23ae35d
Patrice W <phwill2015@gmail.com>
2021-07-05 21:18:08 -0500

    fixed loading issue with page

--- server/index.ts
+++ server/index.ts
@@ -2,13 +2,15 @@ import path from 'path';
 import express from 'express';
 
 const app = express();
-const port = 3000;
+const port = 8000;
 const dist = path.resolve(__dirname, '..', 'client', 'dist');
+
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(dist));
 
-app.listen(3000, () => {
+app.listen(8000, () => {
   console.log(`Server is listening on http://localhost:${port}`);
 });
