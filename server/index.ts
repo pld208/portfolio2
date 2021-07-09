70e8caf96d24d7ad1b5a00fb9b4f8f98b9075888
Patrice W <phwill2015@gmail.com>
2021-07-09 14:46:39 -0500

    gcloud deployment works

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,7 @@ import path from 'path';
 import express from 'express';
 
 const app = express();
-const port = 8000;
+const port = 8080;
 const dist = path.resolve(__dirname, '..', 'client', 'dist');
 
 
@@ -11,6 +11,6 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(dist));
 
-app.listen(8000, () => {
+app.listen(8080, () => {
   console.log(`Server is listening on http://localhost:${port}`);
 });
